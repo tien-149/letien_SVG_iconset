4eb5daf94843310758fd3df47bad081405d935a0
Le Thi Cam Tien <thicamtien.le@users-iMac.localdomain>
2019-01-26 16:15:17 -0500

    add js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(() => { 
+ //self -invoking anonymous function or Lambda}
+ //
+ //
+ //var => mean create a variable
+ //whatever comes after the = is what the variable 
+ var images = document.querySelectorAll('img');
+
+ // document is always a webpage
+ // after the dot is always a method 
+
+ console.log(images);
+})();
